1a8b56349dd7283e7714fd891dc5e8d82ece6415
Arvin mojaverian <arvinmojaverian@hotmail.com>
2020-11-22 11:21:43 +0330

    User Models

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models
+{
+    [Table("User")]
+    class User
+    {
+        public User() : base()
+        {
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+
+    }
+}
