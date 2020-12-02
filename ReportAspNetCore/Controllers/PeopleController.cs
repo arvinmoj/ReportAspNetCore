45467586baf288b40ee2032d67a82c9431000274
Arvin mojaverian <arvinmojaverian@hotmail.com>
2020-12-02 12:40:49 +0330

    Report File Testing

--- ReportAspNetCore/Controllers/PeopleController.cs
+++ ReportAspNetCore/Controllers/PeopleController.cs
@@ -18,7 +18,6 @@ namespace ReportAspNetCore.Controllers
 
         public PeopleController(Data.DatabaseContext context)
         {
-
             _context = context;
             StiLicense.LoadFromString("6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHl2AD0gPVknKsaW0un+3PuM6TTcPMUAWEURKXNso0e5OJN40hxJjK5JbrxU+NrJ3E0OUAve6MDSIxK3504G4vSTqZezogz9ehm+xS8zUyh3tFhCWSvIoPFEEuqZTyO744uk+ezyGDj7C5jJQQjndNuSYeM+UdsAZVREEuyNFHLm7gD9OuR2dWjf8ldIO6Goh3h52+uMZxbUNal/0uomgpx5NklQZwVfjTBOg0xKBLJqZTDKbdtUrnFeTZLQXPhrQA5D+hCvqsj+DE0n6uAvCB2kNOvqlDealr9mE3y978bJuoq1l4UNE3EzDk+UqlPo8KwL1XM+o1oxqZAZWsRmNv4Rr2EXqg/RNUQId47/4JO0ymIF5V4UMeQcPXs9DicCBJO2qz1Y+MIpmMDbSETtJWksDF5ns6+B0R7BsNPX+rw8nvVtKI1OTJ2GmcYBeRkIyCB7f8VefTSOkq5ZeZkI8loPcLsR4fC4TXjJu2loGgy4avJVXk32bt4FFp9ikWocI9OQ7CakMKyAF6Zx7dJF1nZw");
         }
@@ -43,7 +42,7 @@ namespace ReportAspNetCore.Controllers
         {
             StiReport report = new StiReport();
             report.Load(StiNetCoreHelper.MapPath(this, "wwwroot/Reports/Report.mrt"));
-            var persons = _context.Users.ToList();
+            var persons = _context.GetDatas.ToList();
             report.RegData("dt", persons);
             return StiNetCoreViewer.GetReportResult(this, report);
         }
@@ -52,7 +51,7 @@ namespace ReportAspNetCore.Controllers
         {
             StiReport report = new StiReport();
             report.Load(StiNetCoreHelper.MapPath(this, "wwwroot/Reports/Report2.mrt"));
-            var persons = _context.Users.First();
+            var persons = _context.GetDatas.First();
             report.RegData("dt", persons);
             return StiNetCoreViewer.GetReportResult(this, report);
         }
